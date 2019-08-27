ab950d811464d8d2c9804605b1855f9a2a277d88
Bartosz Jarmuz <bjarmuz@sdl.com>
2019-08-27 11:36:49 +0200

    add model

---
+++ src/RepoCat.Portal/Models/ProjectManifest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepoCat.Portal.Models
+{
+    public class ProjectManifest
+    {
+        public string AssemblyName { get; set; }
+        public string ProjectPath { get; set; }
+        public string TargetExt { get; set; }
+        public string OutputType { get; set; }
+    }
+}
