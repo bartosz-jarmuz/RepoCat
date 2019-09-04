830fd2bc5ce5a446eecf92972bf7ee06d73a0f9a
Bartosz Jarmuz <bjarmuz@sdl.com>
2019-09-04 11:54:20 +0200

    allow URL sharing

--- src/RepoCat.Portal/Areas/Catalog/Controllers/SearchController.cs
+++ src/RepoCat.Portal/Areas/Catalog/Controllers/SearchController.cs
@@ -22,7 +22,7 @@ namespace RepoCat.Portal.Areas.Catalog.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var model = new SearchIndexViewModel {Repositories = await this.manifestsService.GetRepositories()};
+            SearchIndexViewModel model = new SearchIndexViewModel {Repositories = await this.manifestsService.GetRepositories()};
 
             return this.View(model);
         }
@@ -31,12 +31,35 @@ namespace RepoCat.Portal.Areas.Catalog.Controllers
         [Route("search")]
         public async Task<PartialViewResult> Search(string repositoryName, string query, bool isRegex)
         {
-            ManifestQueryResult result = await this.manifestsService.FindCurrentProjects(repositoryName, query, isRegex);
-            var queryResultViewModel = this.mapper.Map<ManifestQueryResultViewModel>(result);
-            queryResultViewModel.QueryString = query;
-            queryResultViewModel.IsRegex = isRegex;
+            ManifestQueryResultViewModel queryResultViewModel = await this.GetQueryResultViewModel(repositoryName, query, isRegex);
 
             return this.PartialView("_SearchResultPartial", queryResultViewModel);
+        }
+
+        [HttpGet]
+        [Route("result")]
+        public async Task<IActionResult> GetSearchResultPage(string repositoryName, string query, bool isRegex)
+        {
+            SearchIndexViewModel model = new SearchIndexViewModel
+            {
+                Repositories = await this.manifestsService.GetRepositories(),
+                Repository = repositoryName,
+                Query = query,
+                IsRegex = isRegex
+            };
+
+            ManifestQueryResultViewModel queryResultViewModel = await this.GetQueryResultViewModel(repositoryName, query, isRegex);
+            model.Result = queryResultViewModel;
+            return this.View("Index", model);
+        }
+
+        private async Task<ManifestQueryResultViewModel> GetQueryResultViewModel(string repositoryName, string query, bool isRegex)
+        {
+            ManifestQueryResult result = await this.manifestsService.FindCurrentProjects(repositoryName, query, isRegex);
+            ManifestQueryResultViewModel queryResultViewModel = this.mapper.Map<ManifestQueryResultViewModel>(result);
+            queryResultViewModel.QueryString = query;
+            queryResultViewModel.IsRegex = isRegex;
+            return queryResultViewModel;
         }
     }
 }
--- src/RepoCat.Portal/Areas/Catalog/Models/SearchIndexViewModel.cs
+++ src/RepoCat.Portal/Areas/Catalog/Models/SearchIndexViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RepoCat.Portal.Models.Domain;
 
 namespace RepoCat.Portal.Models
 {
@@ -9,5 +10,8 @@ namespace RepoCat.Portal.Models
     {
         public string Repository { get; set; } 
         public List<string> Repositories { get; set; } = new List<string>();
+        public ManifestQueryResultViewModel Result { get; set; }
+        public string Query { get; set; }
+        public bool IsRegex { get; set; }
     }
 }
