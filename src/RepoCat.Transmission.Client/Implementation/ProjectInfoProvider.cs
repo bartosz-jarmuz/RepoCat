ded9080fc5b5259240b7527d80534f337619e3f8
Bartosz Jarmuz <bjarmuz@sdl.com>
2019-11-01 22:16:21 +0100

    improve error throwing sturcture

--- src/RepoCat.Transmission.Client/Implementation/ProjectInfoProvider.cs
+++ src/RepoCat.Transmission.Client/Implementation/ProjectInfoProvider.cs
@@ -14,6 +14,8 @@ namespace RepoCat.Transmission.Client.Implementation
     {
         private readonly ILog log;
 
+        private readonly string ManifestSuffix = "RepoCat.xml";
+
         public ProjectInfoProvider(ILog log)
         {
             this.log = log;
@@ -43,55 +45,117 @@ namespace RepoCat.Transmission.Client.Implementation
 
         }
 
-
         public ProjectInfo GetInfo(string uri, string repo, string repoStamp)
         {
+            Project project = this.LoadProject(uri);
+            if (project == null)
+            {
+                return null;
+            }
+
             try
             {
-                Project prj = new Project(uri);
-                this.log.Debug($"Project loaded from [{uri}]");
-                var manifestInclude = prj.Items.FirstOrDefault(x =>
-                    x.EvaluatedInclude.EndsWith("RepoCat.xml", StringComparison.CurrentCultureIgnoreCase));
+                var manifestInclude = project.Items.FirstOrDefault(x => x.EvaluatedInclude.EndsWith(this.ManifestSuffix, StringComparison.CurrentCultureIgnoreCase));
                 if (manifestInclude != null)
                 {
-                     this.log.Debug($"Reading Project Info - {uri}");
-
-                    var info = new ProjectInfo()
+                    this.log.Debug($"Reading Project Info - {uri}");
+                    ProjectInfo info = ConstructInfo(uri,repo, repoStamp, project);
+                    if (info != null)
                     {
-                        AssemblyName = prj.Properties.FirstOrDefault(x => x.Name.Equals("AssemblyName", StringComparison.CurrentCultureIgnoreCase))?.EvaluatedValue,
-                        ProjectUri = prj.FullPath,
-                        ProjectName = Path.GetFileNameWithoutExtension(prj.FullPath),
-                        RepositoryName = repo,
-                        RepositoryStamp = repoStamp,
-                        OutputType = prj.Properties.FirstOrDefault(x => x.Name.Equals("OutputType", StringComparison.CurrentCultureIgnoreCase))?.EvaluatedValue,
-                        TargetExtension = prj.Properties.FirstOrDefault(x => x.Name.Equals("TargetExt", StringComparison.CurrentCultureIgnoreCase))?.EvaluatedValue,
-                    };
-
-                    this.log.Debug($"Reading manifest info from {uri}");
-
-                    string manifestPath = Directory.GetFiles(prj.DirectoryPath, manifestInclude.EvaluatedInclude, SearchOption.AllDirectories).FirstOrDefault();
-                    if (string.IsNullOrEmpty(manifestPath))
-                    {
-                        this.log.Warn($"Manifest not found for project {uri}!");
+                        this.log.Debug($"Loaded project info. Reading manifest info from {uri}.");
+                        this.LoadComponentManifest(uri, project, manifestInclude, info);
+                        return info;
                     }
-                    else
-                    {
-                        string manifestContent = File.ReadAllText(manifestPath);
-                        info.Components = ManifestDeserializer.DeserializeComponents(manifestContent);
-                        this.log.Info($"Read OK from {uri}");
-                    }
-                 
-
-                    return info;
+                    
+                }
+                else
+                {
+                    this.log.Debug($"Project does not include manifest file (expected file name ending with [{this.ManifestSuffix}]). Will be ignored by RepoCat. {uri}");
                 }
             }
             catch (Exception ex)
             {
-                this.log.Warn($"Error while loading project info for [{uri}] " +  ex);
+                this.log.Error($"Unexpected error while loading project info for [{uri}] {ex.Message}. Details in DEBUG mode.");
+                this.log.Debug($"Error details: {ex}.");
             }
             return null;
         }
 
-      
+        private void LoadComponentManifest(string uri, Project project, ProjectItem manifestInclude, ProjectInfo info)
+        {
+            try
+            {
+
+                string manifestPath = Directory
+                    .GetFiles(project.DirectoryPath, manifestInclude.EvaluatedInclude, SearchOption.AllDirectories)
+                    .FirstOrDefault();
+                if (string.IsNullOrEmpty(manifestPath))
+                {
+                    this.log.Error($"Manifest not found at [{manifestInclude.EvaluatedInclude}] for project {uri}!");
+                }
+                else
+                {
+                    string manifestContent = File.ReadAllText(manifestPath);
+                    info.Components = ManifestDeserializer.DeserializeComponents(manifestContent);
+                    this.log.Info($"Manifest Read OK from {uri}");
+                }
+            }
+            catch (Exception ex)
+            {
+                this.log.Warn($"Error while loading ComponentManifest [{uri}] {ex.Message}. Details in DEBUG mode.");
+                this.log.Debug($"Error details:" + ex);
+            }
+        }
+
+        private ProjectInfo ConstructInfo(string uri, string repo, string repoStamp, Project prj)
+        {
+            try
+            {
+                var info = new ProjectInfo()
+                {
+                    AssemblyName = prj.Properties
+                        .FirstOrDefault(x => x.Name.Equals("AssemblyName", StringComparison.CurrentCultureIgnoreCase))
+                        ?.EvaluatedValue,
+                    ProjectUri = prj.FullPath,
+                    ProjectName = Path.GetFileNameWithoutExtension(prj.FullPath),
+                    RepositoryName = repo,
+                    RepositoryStamp = repoStamp,
+                    OutputType = prj.Properties
+                        .FirstOrDefault(x => x.Name.Equals("OutputType", StringComparison.CurrentCultureIgnoreCase))
+                        ?.EvaluatedValue,
+                    TargetExtension = prj.Properties
+                        .FirstOrDefault(x => x.Name.Equals("TargetExt", StringComparison.CurrentCultureIgnoreCase))
+                        ?.EvaluatedValue,
+                };
+                return info;
+
+            }
+            catch (Exception ex)
+            {
+                this.log.Warn($"Error while constructing project info based on project object [{uri}] {ex.Message}. Details in DEBUG mode.");
+                this.log.Debug($"Error details:" + ex);
+                return null;
+            }
+
+        }
+
+        private Project LoadProject(string uri)
+        {
+            Project prj;
+            try
+            {
+                prj = new Project(uri);
+                this.log.Debug($"Project loaded from [{uri}]");
+            }
+
+            catch (Exception ex)
+            {
+                this.log.Warn($"Error while loading project [{uri}] {ex.Message}. Details in DEBUG mode.");
+                this.log.Debug($"Error details:" + ex);
+                return null;
+            }
+
+            return prj;
+        }
     }
 }
