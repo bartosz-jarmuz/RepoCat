3e4a978d20d7cfd8dab584cb0b693316ddff3d55
Bartosz Jarmuz <bjarmuz@sdl.com>
2020-01-12 16:48:53 +0100

    toggle the arrows

--- src/RepoCat.Portal/wwwroot/js/site.js
+++ src/RepoCat.Portal/wwwroot/js/site.js
@@ -1,4 +1,16 @@
 ﻿$(document).ready(function () {
+$(function () {
+    $('[data-toggle="collapse"]').click(function(){
+        var icon = $(this).find('.toggler-icon');
+        if (icon.hasClass('icon-arrow-up')){
+                icon.removeClass('icon-arrow-up');
+                icon.addClass('icon-arrow-down');
+        } else{
+               icon.removeClass('icon-arrow-down');
+                icon.addClass('icon-arrow-up');
+        }
+    });
+});
 
 $(function () {
   $('[data-toggle="popover"]').popover()
@@ -32,6 +44,8 @@ $(function () {
             }
         }
     });
+
+
 
     $.get("/Home/NavHeaderStats", function (data) { $("#NavHeaderStatsContainer").replaceWith(function() {
     return $(data).hide().fadeIn(500);
@@ -80,11 +94,12 @@ function collapseAllToggle(sender) {
         $(sender).removeClass('open')
         $('div.collapse').removeClass('show');
         $(sender).text('Expand all')
+        $('[data-toggle="collapse"]').find('.toggler-icon').removeClass('icon-arrow-up').addClass('icon-arrow-down');
     } else {
         $(sender).addClass('open')
         $('div.collapse').addClass('show');
         $(sender).text('Collapse all')
-
+        $('[data-toggle="collapse"]').find('.toggler-icon').removeClass('icon-arrow-down').addClass('icon-arrow-up');
     }
 }
 
