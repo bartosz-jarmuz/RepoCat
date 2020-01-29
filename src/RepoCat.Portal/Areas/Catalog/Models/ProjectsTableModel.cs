c4e7f1fbb410a2567cb8a85b0b46e28b5b32d987
Bartosz Jarmuz <bjarmuz@sdl.com>
2020-01-29 23:41:41 +0100

    break up table into parts and add optional repo column

---
+++ src/RepoCat.Portal/Areas/Catalog/Models/ProjectsTableModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RepoCat.RepositoryManagement.Service;
+using RepoCat.Utilities;
+
+namespace RepoCat.Portal.Areas.Catalog.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProjectsTableModel
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="projects"></param>
+        /// <param name="isMultipleRepositories"></param>
+        public ProjectsTableModel(List<ProjectInfoViewModel> projects, bool isMultipleRepositories)
+        {
+            this.Projects = projects;
+            this.IsMultipleRepositories = isMultipleRepositories;
+        }
+
+        /// <summary>
+        /// Gets or sets the manifests.
+        /// </summary>
+        /// <value>The manifests.</value>
+        public List<ProjectInfoViewModel> Projects { get; internal set; } = new List<ProjectInfoViewModel>();
+     
+        /// <summary>
+        /// Specifies whther the table contains projects from multiple repos
+        /// </summary>
+        public bool IsMultipleRepositories { get; set; }
+    }
+}
